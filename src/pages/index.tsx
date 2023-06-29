20f4443fefa24d56b7f66207e2b0865bea43d14a
s1f102100793 <s1f102100793@iniad.org>
2023-06-29 13:48:22 +0900

    if文の中にif文を入れてみた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,47 +6,68 @@ const Home = () => {
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 0],
+    [0, 2, 0, 1, 2, 2, 2, 0],
+    [0, 0, 0, 2, 2, 2, 2, 0],
+    [0, 0, 0, 0, 0, 2, 2, 0],
+    [0, 0, 0, 0, 0, 2, 2, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0],
   ]);
 
   const onClick = (x: number, y: number) => {
     console.log(x, y);
     const newBoard: number[][] = JSON.parse(JSON.stringify(board));
+    // const directions: number[][] = [
+    //   [-1, -1],
+    //   [-1, 0],
+    //   [-1, 1],
+    //   [0, -1],
+    //   [0, 1],
+    //   [1, 1],
+    //   [1, 0],
+    //   [1, -1],
+    // ]; 
+
     // 下が相手の色だったら置ける
     if (board[y + 1] !== undefined && board[y + 1][x] === 3 - turnColor) {
-      newBoard[y][x] = turnColor;
+      const Y = board.map((key) => key[0]);
+      console.log(Y)
+      if (board[y + 2][x] >= turnColor) {
+        newBoard[y][x] = turnColor;
+        setTurnColor(2 / turnColor);
+      } else {
+        alert('そこは置けねーよバーカ！')
+      }
+      
       // if (turnColor === 1) {
       //   setTurnColor(2);
       // } else {
       //   setTurnColor(1);
       // }
-      setTurnColor(2 / turnColor);
-    } else if (board[y - 1] !== undefined && board[y - 1][x] === 3 - turnColor) {
-      newBoard[y][x] = turnColor;
-      setTurnColor(2 / turnColor);
-    } else if (board[x + 1] !== undefined && board[y][x + 1] === 3 - turnColor) {
-      newBoard[y][x] = turnColor;
-      setTurnColor(2 / turnColor);
-    } else if (board[x - 1] !== undefined && board[y][x - 1] === 3 - turnColor) {
-      newBoard[y][x] = turnColor;
-      setTurnColor(2 / turnColor);
-    } else if (board[y - 1][x - 1] !== undefined && board[y - 1][x - 1] === 3 - turnColor) {
-      newBoard[y][x] = turnColor;
-      setTurnColor(2 / turnColor);
-    } else if (board[y - 1][x + 1] !== undefined && board[y - 1][x + 1] === 3 - turnColor) {
-      newBoard[y][x] = turnColor;
-      setTurnColor(2 / turnColor);
-    } else if (board[y + 1][x - 1] !== undefined && board[y + 1][x - 1] === 3 - turnColor) {
-      newBoard[y][x] = turnColor;
-      setTurnColor(2 / turnColor);
-    } else if (board[y + 1][x + 1] !== undefined && board[y + 1][x + 1] === 3 - turnColor) {
-      newBoard[y][x] = turnColor;
-      setTurnColor(2 / turnColor);
+    // } else if (board[y - 1] !== undefined && board[y - 1][x] === 3 - turnColor && board[y - 2][x] === 2 - turnColor) {
+    //   newBoard[y][x] = turnColor;
+    //   setTurnColor(2 / turnColor);
+    // } else if (board[x + 1] !== undefined && board[y][x + 1] === 3 - turnColor) {
+    //   newBoard[y][x] = turnColor;
+    //   setTurnColor(2 / turnColor);
+    // } else if (board[x - 1] !== undefined && board[y][x - 1] === 3 - turnColor) {
+    //   newBoard[y][x] = turnColor;
+    //   setTurnColor(2 / turnColor);
+    // } else if (board[y - 1][x - 1] !== undefined && board[y - 1][x - 1] === 3 - turnColor) {
+    //   newBoard[y][x] = turnColor;
+    //   setTurnColor(2 / turnColor);
+    // } else if (board[y - 1][x + 1] !== undefined && board[y - 1][x + 1] === 3 - turnColor) {
+    //   newBoard[y][x] = turnColor;
+    //   setTurnColor(2 / turnColor);
+    // } else if (board[y + 1][x - 1] !== undefined && board[y + 1][x - 1] === 3 - turnColor) {
+    //   newBoard[y][x] = turnColor;
+    //   setTurnColor(2 / turnColor);
+    // } else if (board[y + 1][x + 1] !== undefined && board[y + 1][x + 1] === 3 - turnColor) {
+    //   newBoard[y][x] = turnColor;
+    //   setTurnColor(2 / turnColor);
+    // } 
+    } else {
+      alert('そこは置けねーよバーカ！')
     }
 
     // turnColor === setTurnColor(1) setTurnColor(2);
